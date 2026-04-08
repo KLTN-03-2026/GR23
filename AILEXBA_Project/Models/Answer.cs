8a76bef839e094c6b52c69bcc18aea4c1239bdef
vanquoc06 <vanquoc11535@gmail.com>
2026-04-09 01:46:22 +0700

    Update Answer.cs

--- AILEXBA_Project/Models/Answer.cs
+++ AILEXBA_Project/Models/Answer.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization; // Thư viện để xử lý JSON
 
 namespace AILEXBA_Project.Models
 {
@@ -6,10 +7,14 @@ namespace AILEXBA_Project.Models
     {
         [Key]
         public int Id { get; set; }
+
         public string Text { get; set; } = string.Empty;
+
         public bool IsCorrect { get; set; } // Trả lời đúng hay sai
 
         public int QuestionId { get; set; }
-        public Question ? Question { get; set; } // Liên kết ngược lại Câu hỏi
+
+        [JsonIgnore]
+        public Question? Question { get; set; } // Chặn không cho JSON quay ngược lại Question
     }
 }
