3f566ebde84218a5219f20d02795cbf7e99f1710
vanquoc06 <vanquoc11535@gmail.com>
2026-05-09 17:54:30 +0700

    Create ExplainRequest.cs

---
+++ alilexba_backend/DTOs/ExplainRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace alilexba_backend.DTOs
+{
+    public class ExplainRequest
+    {
+        public string Question { get; set; } = string.Empty;
+        public string UserAnswer { get; set; } = string.Empty;
+        public string CorrectAnswer { get; set; } = string.Empty;
+    }
+}
